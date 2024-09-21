44300831f7958a7ba18a8a10e43af9ac72036b66
Deepak Chhantyal <deepakchhantyal4156@gmail.com>
2024-09-21 15:12:43 +0545

    doneprofilecard

---
+++ src/Component/UserProfileCard/UserProfileCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import profile_icon from '../Assets/elon.jpeg'
+import './UserProfileCard.css'
+
+const UserProfileCard = () => {
+  return (
+    <div className='upc'>
+        <div className="gradiant"></div>
+        <div className="profile-down">
+            <img src={profile_icon} alt="" />
+            <div className="profile-title">Elon Musk</div>
+            <div className="profile-description">
+                I am the founder, Chairman & Cheif executive officer of Space X, CEO of Tesla , Product officier of the Tesla
+            </div>
+        </div>
+        <div className="profile-button"><a href="mailto:deepakchhantyal4156@gmail.com">Contact Menu</a></div>
+    </div>
+  )
+}
+
+
+export default UserProfileCard
